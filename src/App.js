4ccf88202b3d7ef5b8096f641fca2e356a11f597
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2021-03-25 19:58:32 +0530

    Navigation-dispatch-actions-app.js-custom-header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,98 @@
-import React from 'react'
-import {StyleSheet,Text} from 'react-native'
+import React,{useEffect} from 'react'
+import {Text} from 'react-native'
+import 'react-native-gesture-handler'
 
-const App =()=>{
+import auth from '@react-native-firebase/auth'
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+import {useDispatch,connect} from 'react-redux'
+
+import AddPost from './screens/AddPost'
+import SignIn from './screens/SignIn'
+import SihnUp from './screens/SignUp'
+import Home from './screens/Home'
+
+import CustomHeader from './layout/CustomHeader'
+
+import {SET_USER,IS_AUTHETHENTICATED} from './action/action.types'
+
+import database from '@react-native-firebase/database'
+import EmptyContainer from './components/EmptyContainer'
+import {requestPermission} from './utils/AskPermission'
+
+const Stack = createStackNavigator();
+
+const App =({authState})=>{
+
+    const dispatch =useDispatch();
+
+    const onAuthStateChanged =(user)=>{
+        if(user){
+            dispatch({
+                type:IS_AUTHETHENTICATED,
+                payload:true
+            })
+
+            console.log(user._user.uid)
+
+            database()
+            .ref(`/users/${user._user.uid}`)
+            .on('value',(snapshot)=>{
+                console.log('USER DETAILS',snapshot.val())
+                dispatch({
+                    type:SET_USER,
+                    payload:snapshot.val(),
+                })
+            })
+        }else{
+            dispatch({
+                type:IS_AUTHETHENTICATED,
+                payload:false
+            })
+        }
+    }
+
+    useEffect(()=>{
+        requestPermission()
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
+        return subscriber
+    },[])
+
+    if(authState.loading){
+        return <EmptyContainer/>
+    }
+
     return(
         <>
-        <Text>Hello From App.js</Text>
+        <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                  header:(props)=><CustomHeader {...props}/>
+              }}
+            >
+                {
+                    authState.isAuthenticated ? (
+                        <>
+                        <Stack.Screen name="Home" component={Home}/>
+                        <Stack.Screen name="AddPost" component={AddPost}/>
+                        </>
+                    ):(
+                        <>
+                        <Stack.Screen name="SignIn" component={SignIn}/>
+                        <Stack.Screen name="SignUp" component={SihnUp}/>
+                        </>
+                    )
+                }
+            </Stack.Navigator>
+        </NavigationContainer>
         </>
     )
 }
 
-export default App
+const mapStateToProps =(state)=>({
+    authState:state.auth
+})
+
+export default connect(mapStateToProps)(App)
